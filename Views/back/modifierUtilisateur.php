ecb85bd2e1a9d384e53c0b360db13775eb95c761
MAR14HL <soulaima.hleli@esprit.tn>
2023-11-27 14:13:41 +0100

    changes

---
+++ Views/back/modifierUtilisateur.php
@@ -0,0 +1,346 @@
+<?php
+include_once '../../Model/Utilisateur.php';
+include_once '../../Controller/UtilisateurC.php';
+
+session_start() ;
+
+if (isset($_SESSION["username"]))
+{
+    if ($_SESSION["role_user"] == "USER_ROLE" )        header("location:../front/index.php") ;
+} else {
+    header("location:../front/index.php") ;
+}
+
+$utilisateurC = new UtilisateurC();
+if (isset($_GET['id'])){
+    $utilisateur = $utilisateurC->getUtilisateurById($_GET['id']);
+}
+if (
+    isset($_POST["nom_user"]) &&
+    isset($_POST["prenom_user"]) &&
+    isset($_POST["email_user"]) &&
+    isset($_POST["tel_user"]) &&
+    isset($_POST["adresse_user"]) &&
+    isset($_POST["username"]) &&
+    isset($_POST["password_user"]) &&
+    isset($_POST["role_user"])
+) {
+
+    if (
+        !empty($_POST["nom_user"]) &&
+        !empty($_POST["prenom_user"]) &&
+        !empty($_POST["email_user"]) &&
+        !empty($_POST["tel_user"]) &&
+        !empty($_POST["adresse_user"]) &&
+        !empty($_POST["username"]) &&
+        !empty($_POST["password_user"]) &&
+        !empty($_POST["role_user"])
+    ){
+        $nom_user = $_POST['nom_user'] ;
+        $prenom_user = $_POST['prenom_user'] ;
+        $email_user = $_POST['email_user'] ;
+        $tel_user = $_POST['tel_user'] ;
+        $adresse_user = $_POST['adresse_user'] ;
+        $username = $_POST['username'] ;
+        $password_user = md5($_POST['password_user']) ;
+        $role_user = $_POST['role_user'] ;
+
+        $utilisateur = new Utilisateur($nom_user,
+            $prenom_user,
+            $email_user,
+            $tel_user,
+            $adresse_user,
+            $username,
+            $password_user,
+            $role_user
+        );
+
+        $utilisateurC->modifier_Utilisateur($utilisateur,$_POST['id_user']);
+        header('Location: Utilisateurs.php');
+    }
+
+
+
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Dashboard - SB Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <style>
+        .error {
+            color: red;
+            font-size: 14px;
+        }
+    </style>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+            </div>
+        </form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#!">Settings</a></li>
+                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="#!">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Admin Dashboard</div>
+                        <a class="nav-link" href="index.html">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
+                        <a class="nav-link" href="livraisons.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Livraison
+                        </a>
+                        <a class="nav-link" href="typelivraisons.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            types de Livraison
+                        </a>
+
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Start Bootstrap
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Utilisateurs</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item active">Nouveau utilisateur</li>
+                    </ol>
+
+                    <div class="container mt-5">
+                        <form method="POST" onsubmit="return validateForm()">
+                        <div class="form-group">
+                        <div class="form-group">
+                                        <label for="exampleInputUsername1">id</label>
+                                        <input type="text" class="form-control" name="id_user" id="id" value="<?php echo $utilisateur['id_user']; ?>"   readonly>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Nom</label>
+                                        <input type="text" name="nom_user" class="form-control" id="nom_user" value="<?php echo $utilisateur['nom_user']; ?>">
+                                        <div id="error_nom_user" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">prenom</label>
+                                        <input type="text" class="form-control" name="prenom_user" id="prenom_user" value="<?php echo $utilisateur['prenom_user']; ?>" placeholder="prenom user">
+                                        <div id="error_prenom_user" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">email</label>
+                                        <input type="email" name="email_user" class="form-control" id="email_user" value="<?php echo $utilisateur['email_user']; ?>" >
+                                        <div id="error_email_user" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">tel</label>
+                                        <input type="tel" class="form-control" name="tel_user" id="tel_user" value="<?php echo $utilisateur['tel_user']; ?>" >
+                                        <div id="error_tel_user" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">adresse</label>
+                                        <input type="text" class="form-control" name="adresse_user" id="adresse_user" value="<?php echo $utilisateur['adresse_user']; ?>" >
+                                        <div id="error_adresse_user" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">adresse</label>
+                                        <input type="text" class="form-control" name="username" id="username" value="<?php echo $utilisateur['username']; ?>" >
+                                        <div id="error_username" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">password</label>
+                                        <input type="password" class="form-control" name="password_user" id="password_user" value="<?php echo $utilisateur['password_user']; ?>" >
+                                        <div id="error_password_user" class="error"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">role</label>
+                                        <select class="form-control" name = "role_user">
+                                            <option value="USER_ROLE">user</option>
+                                            <option value="ADMIN_ROLE">admin</option>
+                                        </select>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary me-2" name="submitbtn" >Submit</button>
+                                    <button class="btn btn-light">Cancel</button>
+                            <a href="Utilisateurs.php"><button class="btn btn-danger" type="button">Retour à la liste</button></a>
+                        </form>
+                    </div>
+                </div>
+            </main>
+
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+    <script>
+        function validateForm() {
+            var idPartenaire = document.getElementById('idPartenaire').value;
+            var date = document.getElementById('date').value;
+            var livraison = document.getElementById('livraison').value;
+            var type = document.getElementById('type').value;
+            var adresse = document.getElementById('adresse').value;
+            var status = document.getElementById('status').value;
+            var numero = document.getElementById('numero').value;
+
+            // Vérifiez ici vos conditions de validation
+            if (idPartenaire === '' || date === '' || livraison === '' || type === '' || adresse === '' || status === '' || numero === '') {
+                alert('Veuillez remplir tous les champs.');
+                return false;
+            }
+
+
+            return true;
+        }
+    </script>
+     <script>
+        function validateForm() {
+            // Reset error messages
+            resetErrors();
+
+            // Validation logic for each field
+            var isValid = true;
+
+            // Validate Nom
+            var nomUser = document.getElementById('nom_user').value.trim();
+            if (nomUser === '') {
+                displayError('error_nom_user', 'Nom is required');
+                isValid = false;
+            }
+
+            // Validate Prenom
+            var prenomUser = document.getElementById('prenom_user').value.trim();
+            if (prenomUser === '') {
+                displayError('error_prenom_user', 'Prenom is required');
+                isValid = false;
+            }
+
+            // Validate Email
+            var emailUser = document.getElementById('email_user').value.trim();
+            if (emailUser === '' || !isValidEmail(emailUser)) {
+                displayError('error_email_user', 'Enter a valid email address');
+                isValid = false;
+            }
+
+            // Validate Tel
+            var telUser = document.getElementById('tel_user').value.trim();
+            if (telUser === '' || !isValidPhoneNumber(telUser)) {
+                displayError('error_tel_user', 'Enter a valid phone number');
+                isValid = false;
+            }
+
+            // Validate Adresse
+            var adresseUser = document.getElementById('adresse_user').value.trim();
+            if (adresseUser === '') {
+                displayError('error_adresse_user', 'Adresse is required');
+                isValid = false;
+            }
+
+            // Validate Username
+            var username = document.getElementById('username').value.trim();
+            if (username === '') {
+                displayError('error_username', 'Username is required');
+                isValid = false;
+            }
+
+            // Validate Password
+            var passwordUser = document.getElementById('password_user').value.trim();
+            if (passwordUser === '') {
+                displayError('error_password_user', 'Password is required');
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        function resetErrors() {
+            var errorDivs = document.getElementsByClassName('error');
+            for (var i = 0; i < errorDivs.length; i++) {
+                errorDivs[i].innerText = '';
+            }
+        }
+
+        function displayError(id, message) {
+            var errorDiv = document.getElementById(id);
+            if (errorDiv) {
+                errorDiv.innerText = message;
+            }
+        }
+
+        function isValidEmail(email) {
+            // Basic email validation, you can enhance it as needed
+            var emailRegex = /\S+@\S+\.\S+/;
+            return emailRegex.test(email);
+        }
+
+        function isValidPhoneNumber(phoneNumber) {
+            // Basic phone number validation, you can enhance it as needed
+            var phoneRegex = /^\d{10}$/;
+            return phoneRegex.test(phoneNumber);
+        }
+    </script>
+
+</body>
+
+</html>
--- view/back/deleteuser.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../Controller/usercontroller.php';
-$user = new usercontroller();
-$user->deleteuser($_GET["id"]);
-header('Location:listuser.php');
